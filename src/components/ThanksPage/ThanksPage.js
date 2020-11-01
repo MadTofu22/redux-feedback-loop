4ddc1c6fc84bbe1c288731e5272b3e863098815d
MadTofu22 <stutler.tom@gmail.com>
2020-11-01 16:05:55 -0600

    axios get request in thankspage setupp then commented it out cause thats for admin.....

--- src/components/ThanksPage/ThanksPage.js
+++ src/components/ThanksPage/ThanksPage.js
@@ -5,16 +5,30 @@ import axios from 'axios';
 
 class ThanksPage extends Component {
 
-    state = {
-        feedbackData: []
-    }
+    // state = {
+    //     feedbackData: []
+    // }
 
-    
+    // getFeedback = () => {
+    //     axios.get('/feedback').then(response => {
+    //         this.setState({
+    //             feedbackData: response.data
+    //         });
+    //     }).catch(error => {
+    //         console.log(error);
+    //     });
+    // }
+
+    // This function handles the click event for the leave new feedback button
+    handleClick = () => {
+        
+    }
 
     render () {
         return (
             <>
-                
+                <h2>Thank You!</h2>
+                <button onClick={this.handleClick}>Leave New Feedback</button>
             </>
         );
     }
